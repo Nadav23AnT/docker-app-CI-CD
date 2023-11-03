23665757de3daf85bd08808b98c55cb8043c552c
Nadav23AnT <nadav2282@gmail.com>
2023-11-03 18:23:44 +0200

    configure an AMI image in AWS

--- datasources.tf
+++ datasources.tf
@@ -1,9 +1,9 @@
 data "aws_ami" "server_ami" {
   most_recent = true
-  owners      = ["099720109477"]
+  owners      = ["179496348720"]
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["179496348720/jenkins-node-docker-sample-app"]
   }
 }
